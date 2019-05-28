99bc1f208f0c8b78e4dee594258c0cc379736630
Natalia Rodriguez <32943728+nrodriguez99@users.noreply.github.com>
2019-05-28 00:07:15 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/apprecetas/View/LoginActivity.java
    #	app/src/main/java/com/example/apprecetas/View/MenuActivity.java
    #	app/src/main/java/com/example/apprecetas/View/SearchActivity.java

--- app/src/main/java/com/example/apprecetas/View/RecipeActivity.java
+++ app/src/main/java/com/example/apprecetas/View/RecipeActivity.java
@@ -5,17 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 import com.example.apprecetas.R;
 
+import java.util.ArrayList;
+
 public class RecipeActivity extends AppCompatActivity {
 
-    int[] imagenes = {R.drawable.pizza,R.drawable.dishes,R.drawable.enano};
+    //int[] imagenes = {R.drawable.pizza,R.drawable.dishes,R.drawable.enano};
+    private ArrayList imagenes = new ArrayList();
     private int index = 0;
 
     @Override
@@ -23,6 +26,36 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
+        String nombre = "";
+        String tipo= "";
+        String ingredientes="";
+        String instrucciones="";
+        //ArrayList imagenes ;
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null){
+            nombre = extras.getString("nombre");
+            tipo = extras.getString("tipo");
+            ingredientes = extras.getString("ingredientes");
+            instrucciones = extras.getString("instrucciones");
+            imagenes = extras.getStringArrayList("imagenes");
+        }
+        //ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+        //Picasso.get().load(gettingImageUrl).into(imageView1);
+
+        TextView tvNombre = (TextView) findViewById(R.id.tvNombre);
+        tvNombre.setText(nombre);
+
+        TextView tvTipo = (TextView) findViewById(R.id.tvTipo);
+        tvTipo.setText(tipo);
+
+        TextView tvListaIngredinetes = (TextView) findViewById(R.id.tvListaIngredientes);
+        tvListaIngredinetes.setText(ingredientes.replace("[","").replace("]",""));
+
+        TextView tvListaInstrucciones = (TextView) findViewById(R.id.tvListaInstrucciones);
+        tvListaInstrucciones.setText(instrucciones.replace("[","").replace("]",""));
+
+
+        //Imagenes
         final ImageSwitcher imageSwitcher = findViewById(R.id.imageSwitcher);
         if(imageSwitcher != null){
             imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
@@ -36,7 +69,7 @@ public class RecipeActivity extends AppCompatActivity {
                     return imageView;
                 }
             });
-            imageSwitcher.setImageResource(imagenes[index]);
+            //imageSwitcher.setImageResource(Picasso.get().load(imagenes.get(index)));
 
             Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
             imageSwitcher.setInAnimation(in);
@@ -44,17 +77,17 @@ public class RecipeActivity extends AppCompatActivity {
             Animation out = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
             imageSwitcher.setOutAnimation(out);
         }
-        Button button = findViewById(R.id.btnNext);
+        /*Button button = findViewById(R.id.btnNext);
         if(button != null){
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    index = (++index < imagenes.length) ? index : 0;
+                    index = (++index < imagenes.size()) ? index : 0;
                     if(imageSwitcher != null){
-                        imageSwitcher.setImageResource(imagenes[index]);
+                        imageSwitcher.setImageResource(imagenes.get(index));
                     }
                 }
             });
-        }
+        } */
     }
 }
